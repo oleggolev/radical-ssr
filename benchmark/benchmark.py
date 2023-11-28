1f4ea8d41643cbbe7f998ef0683f016618c38e81
Oleg Golev <ogolev@princeton.edu>
2023-11-28 15:57:45 -0500

    new benchmark code and results w/o RADICAL

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -40,23 +40,23 @@ def time_get_posts():
 
 def measure_latencies():
     first_load = time_get_posts()
-    cached_loads = [0] * 10
-    for i in range(10):
-        cached_loads[i] = time_get_posts()
-    cached_load = sum(cached_loads) / len(cached_loads)
+    cached_load  = time_get_posts()
     return first_load, cached_load
 
-# Measure latency with a pre-determined set of N posts.
+# Create a session and cache the connection.
+session = requests.Session()
+session.get(target + "/about")
+
+# Prepare the output CSV file for writing results.
 fs_output = open('benchmark/' + args.outfile, 'w')
-num_trials = 10
-N_set = [0, 5, 10, 15, 20, 50]
+fs_output.write(f"num posts, initial_load_latency, cached_load_latency\n")
+
+# Measure latency with a pre-determined set of N posts.
+N_set = [0, 5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 for N in N_set:
+    num_trials = 10
     first_load_outputs = [0] * num_trials
     cached_load_outputs = [0] * num_trials
-    
-    # Cache connection.
-    session = requests.Session()
-    session.get(target + "/about")
     
     # Run the trials.
     for trial_num in range(num_trials):
@@ -67,6 +67,9 @@ for N in N_set:
         first_load, cached_load = measure_latencies()
         first_load_outputs[trial_num] = first_load
         cached_load_outputs[trial_num] = cached_load
-    fs_output.write(f"{N}: {sum(first_load_outputs) / len(first_load_outputs)}, {sum(cached_load_outputs) / len(cached_load_outputs)}\n")
-    
+        
+    # Write the average load times for this N.
+    fs_output.write(f"{N}, {sum(first_load_outputs) / len(first_load_outputs)}, {sum(cached_load_outputs) / len(cached_load_outputs)}\n")
+
+# Done.
 fs_output.close()   
